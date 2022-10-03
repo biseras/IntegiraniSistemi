f5b69fdbd844d8c1ce9dc0fe68f15e03222080db
mariopetkovskii <petkovski_mario@yahoo.com>
2022-10-03 02:33:52 +0200

    changes

--- Movie.Domain/Models/MovieA.cs
+++ Movie.Domain/Models/MovieA.cs
@@ -9,10 +9,12 @@ namespace Movie.Domain.Models
         public String Title { get; set; }
         public int Year { get; set; }
         public String Rated { get; set; }
+        public String imdbRating { get; set; }
+        public String imdbVotes { get; set; }
         public String Released { get; set; }
         public String Runtime { get; set; }
-        public String Genre { get; set; } 
-        public String Director{ get; set; }
+        public String Genre { get; set; }
+        public String Director { get; set; }
         public String Writer { get; set; }
         public String Actors { get; set; }
         public String Plot { get; set; }
